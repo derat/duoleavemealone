48806ad366c832165c71c7eda4422921f8aa5db9
Daniel Erat <dan@erat.org>
2019-09-08 17:17:44 -0400

    More refinements, and click through end screens.

--- content_script.js
+++ content_script.js
@@ -2,23 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Returns the <button> element that's used to advance to the next screen.
-// The same button is used for correct and incorrect answers, motivational
-// messages, etc.
-function findNextButton() {
-  for (const e of document.getElementsByTagName('button')) {
-    if (e.getAttribute('data-test') == 'player-next') return e;
+// Finds an element of type |tagName| with the supplied 'data-test' attribute.
+function findElement(tagName, dataTest) {
+  for (const e of document.getElementsByTagName(tagName)) {
+    if (e.getAttribute('data-test') == dataTest) return e;
   }
   return null;
 }
 
 // Returns true if it looks like the user has just answered a question
 // correctly.
-function haveCorrectAnswer() {
+function answeredCorrectly() {
   for (const e of document.getElementsByTagName('h2')) {
     if (
       e.innerText == 'You are correct' ||
-      e.innerText.startsWith('Another correct solution:')
+      e.innerText.startsWith('Another correct solution:') ||
+      e.innerText.startsWith('Pay attention to the accents.')
     ) {
       return true;
     }
@@ -26,29 +25,66 @@ function haveCorrectAnswer() {
   return false;
 }
 
-// The button isn't there initially, so poll for it until it appears.
-const timer = setInterval(() => {
-  const b = findNextButton();
-  if (!b) return;
-
-  console.log('Found next button');
-  clearTimeout(timer);
-
-  new MutationObserver(mutations => {
-    let isContinue = false;
-    for (const m of mutations) {
-      if (m.type == 'characterData' && m.target.data == 'Continue') {
-        isContinue = true;
-        break;
-      }
+// Returns true if it looks like a lesson-complete screen is being displayed.
+function lessonComplete() {
+  for (const e of document.getElementsByTagName('h2')) {
+    if (
+      e.innerText.startsWith('Lesson Complete!') ||
+      e.innerText.startsWith('Combo bonus!')
+    ) {
+      return true;
     }
-
-    if (isContinue && haveCorrectAnswer()) {
-      console.log('Continuing after correct answer');
-      b.click();
+  }
+  for (const e of document.getElementsByTagName('a')) {
+    if (e.innerText == 'Find events near you') {
+      return true;
     }
-  }).observe(b, {
+  }
+  return false;
+}
+
+let nextButton = null;
+
+const observer = new MutationObserver(mutations => {
+  if (!nextButton) return;
+
+  if (answeredCorrectly()) {
+    console.log('Continuing after correct answer');
+    nextButton.click();
+  } else if (lessonComplete()) {
+    console.log('Continuing after lesson complete');
+    nextButton.click();
+  }
+});
+
+// Observe the <button> element that's used to advance to the next screen. The
+// same button is used for correct and incorrect answers, motivational messages,
+// etc. The button isn't there initially, so poll for it until it appears.
+const nextButtonTimer = setInterval(() => {
+  nextButton = findElement('button', 'player-next');
+  if (!nextButton) return;
+
+  console.log('Watching next button');
+  clearTimeout(nextButtonTimer);
+  observer.observe(nextButton, {
     subtree: true,
+    // The text of the button changes to 'Continue' when it becomes clickable.
     characterData: true,
+  });
+}, 100);
+
+// Observe the <div> element that's displayed when the lesson is complete.
+const endCarouselTimer = setInterval(() => {
+  const carousel = findElement('div', 'player-end-carousel');
+  if (!carousel) return;
+
+  console.log('Watching end carousel');
+  clearTimeout(endCarouselTimer);
+  observer.observe(carousel, {
+    // There are a bunch of nested elements inside of this div that are used for
+    // the different screens. The all appear to be present initially, so we
+    // watch for attribute changes to get signaled when they're moved onscreen.
+    attributes: true,
+    subtree: true,
   });
 }, 100);
