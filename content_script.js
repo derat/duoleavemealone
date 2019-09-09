972b41db1434f24af0228f72fd5db4fb37c88a58
Daniel Erat <dan@erat.org>
2019-09-09 10:12:17 -0400

    Observe all mutations instead of using timers.

--- content_script.js
+++ content_script.js
@@ -10,114 +10,81 @@ function findElement(tagName, f) {
   return null;
 }
 
-// This class interacts with the skill page.
-class SkillHandler {
-  constructor() {
-    this.observer = new MutationObserver(mutations => {
-      if (!this.nextButton) return;
-
-      if (this.answeredCorrectly()) {
-        console.log('Continuing after correct answer');
-        this.nextButton.click();
-      } else if (this.lessonComplete()) {
-        console.log('Continuing after lesson complete');
-        this.nextButton.click();
-      }
-    });
-
-    // Observe the <button> element that's used to advance to the next screen. The
-    // same button is used for correct and incorrect answers, motivational messages,
-    // etc. The button isn't there initially, so poll for it until it appears.
-    this.nextButtonTimer = setInterval(() => {
-      this.nextButton = findElement(
-        'button',
-        e => e.getAttribute('data-test') == 'player-next',
-      );
-      if (!this.nextButton) return;
-
-      clearTimeout(this.nextButtonTimer);
-      this.nextButtonTimer = null;
-
-      console.log('Watching next button');
-      this.observer.observe(this.nextButton, {
-        subtree: true,
-        // The text of the button changes to 'Continue' when it becomes clickable.
-        characterData: true,
-      });
-    }, 100);
-
-    // Observe the <div> element that's displayed when the lesson is complete.
-    this.endCarouselTimer = setInterval(() => {
-      const carousel = findElement(
-        'div',
-        e => e.getAttribute('data-test') == 'player-end-carousel',
-      );
-      if (!carousel) return;
-
-      clearTimeout(this.endCarouselTimer);
-      this.endCarouselTimer = null;
-
-      console.log('Watching end carousel');
-      this.observer.observe(carousel, {
-        // There are a bunch of nested elements inside of this div that are used for
-        // the different screens. The all appear to be present initially, so we
-        // watch for attribute changes to get signaled when they're moved onscreen.
-        attributes: true,
-        subtree: true,
-      });
-    }, 100);
-  }
-
-  // Cleans up resources.
-  stop() {
-    this.observer.disconnect();
-    if (this.nextButtonTimer) clearTimeout(this.nextButtonTimer);
-    if (this.endCarouselTimer) clearTimeout(this.endCarouselTimer);
-  }
-
-  // Returns true if it looks like the user has just answered a question
-  // correctly.
-  answeredCorrectly() {
-    return findElement(
-      'h2',
-      e =>
-        e.innerText == 'You are correct' ||
-        e.innerText.startsWith('Another correct solution:') ||
-        e.innerText.startsWith('Pay attention to the accents.'),
-    );
-  }
-
-  // Returns true if it looks like a lesson-complete screen is being displayed.
-  lessonComplete() {
-    return (
-      findElement(
-        'h2',
-        e =>
-          e.innerText.startsWith('Lesson Complete!') ||
-          e.innerText.startsWith('Combo bonus!'),
-      ) || findElement('a', e => e.innerText == 'Find events near you')
-    );
-  }
+// Returns true if it looks like the user has just answered a question
+// correctly.
+function answeredCorrectly() {
+  return !!findElement(
+    'h2',
+    e =>
+      e.innerText == 'You are correct' ||
+      e.innerText.startsWith('Another correct solution:') ||
+      e.innerText.startsWith('Pay attention to the accents.'),
+  );
 }
 
-let skillHandler = null;
+// Returns true if a motivational screen (e.g. Duolingo saying "Excellent!") is
+// shown.
+function motivationShown() {
+  // This is pretty crappy, but I can't find any other way to match it.
+  // Hardcoding all of the messages doesn't seem great, and I don't see any
+  // other attributes that can be used to find this.
+  return !!findElement('div', e => {
+    const img = getComputedStyle(e)['background-image'];
+    return img && img.indexOf('/owls/') != -1;
+  });
+}
 
-// This is disgusting and wasteful. Navigation between pages looks like it
-// happens using history.pushState, which means that our content script won't be
-// loaded if we just mach /skill URLs. So, we instead run in the whole domain
-// and poll the URL to determine when we're on a skill page. I think that the
-// "right" way to handle this is to also have a background page that uses
-// something like chrome.webNavigation to watch for changes. Note that the
-// skill-related code is already terrible and polls in order to watch for the
-// next buttond and end carousel. :-/
-const locationTimer = setInterval(() => {
-  const inSkill = window.location.href.indexOf('/skill/') != -1;
-  if (inSkill && !skillHandler) {
-    console.log('Entered skill page');
-    skillHandler = new SkillHandler();
-  } else if (!inSkill && skillHandler) {
-    console.log('Left skill page');
-    skillHandler.stop();
-    skillHandler = null;
+// Returns true if it looks like a lesson-complete screen is being displayed.
+function lessonComplete() {
+  return !!(
+    findElement(
+      'h2',
+      e =>
+        e.innerText.startsWith('Lesson Complete!') ||
+        e.innerText.startsWith('Combo bonus!'),
+    ) || findElement('a', e => e.innerText == 'Find events near you')
+  );
+}
+
+let nextButton = null;
+
+// It looks like Duolingo uses history.pushState to navigate between pages, so
+// we can't just run the script on /skill/ URLs. I don't think that there's any
+// way to detect pushState navigations from within a content script. Rather than
+// adding an additional background script that uses chrome.webNavigation API and
+// communicates with the content script, we just observe all DOM changes across
+// the main site.
+new MutationObserver(mutations => {
+  if (window.location.href.indexOf('/skill/') == -1) {
+    if (nextButton) {
+      console.log('Left skill page');
+      nextButton = null;
+    }
+    return;
   }
-}, 1000);
+
+  if (nextButton == null) {
+    nextButton = findElement(
+      'button',
+      e => e.getAttribute('data-test') == 'player-next',
+    );
+    if (nextButton) console.log('Found next button');
+  }
+
+  if (!nextButton || nextButton.innerText != 'CONTINUE') return;
+
+  if (answeredCorrectly()) {
+    console.log('Continuing after correct answer');
+    nextButton.click();
+  } else if (motivationShown()) {
+    console.log('Continuing through motivational message');
+    nextButton.click();
+  } else if (lessonComplete()) {
+    console.log('Continuing after lesson complete');
+    nextButton.click();
+  }
+}).observe(document, {
+  attributes: true,
+  characterData: true,
+  subtree: true,
+});
