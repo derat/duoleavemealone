derat/duoleavemealone

commit c7d911a49c2d632771d709cb53ab734ea442cd56

Bump to 0.9 and skip skill-level test start screens.
Automatically skip the start screen displayed before testing
out of the current level of a skill (triggered by clicking
the "key" icon in the bubble that pops up after clicking on
the skill).

Fixes #13.

content.js
@@ -144,10 +144,11 @@ class ButtonClicker {
 
     const isPractice = window.location.href.indexOf('/practice') != -1;
     const isSkill = window.location.href.indexOf('/skill/') != -1;
-    const isTest = window.location.href.indexOf('/bigtest/') != -1;
+    const isSkillTest = isSkill && window.location.href.endsWith('/test');
+    const isBigTest = window.location.href.indexOf('/bigtest/') != -1;
     const isCheckpoint = window.location.href.indexOf('/checkpoint/') != -1;
 
-    if (!isPractice && !isSkill && !isTest && !isCheckpoint) {
+    if (!isPractice && !isSkill && !isBigTest && !isCheckpoint) {
       if (this.nextButton) {
         console.log('Left practice/skill/test/checkpoint page');
         this.nextButton = null;
@@ -225,14 +226,27 @@ class ButtonClicker {
       }
     }
 
-    // Auto-start tests and checkpoints.
+    // Auto-start "big tests" and checkpoints.
     if (
-      (isTest || isCheckpoint) &&
+      (isBigTest || isCheckpoint) &&
       this.numCorrectClicks == 0 &&
       buttonColor == greenButtonColor &&
       findElements('img', e => e.src.indexOf('/checkpoint-castle') != -1)
     ) {
-      console.log('Skipping test/checkpoint start screen');
+      console.log('Skipping big-test/checkpoint start screen');
+      this.nextButton.click();
+      return;
+    }
+
+    // Auto-start tests used to skip ahead to the next level in a skill (i.e.
+    // the "key" icon).
+    if (
+      isSkillTest &&
+      this.numCorrectClicks == 0 &&
+      buttonColor == greenButtonColor &&
+      findElements('div', e => e.getAttribute('data-test') == 'skill-icon')
+    ) {
+      console.log('Skipping skill test start screen');
       this.nextButton.click();
       return;
     }

commit 9976f4cb068f8df7ec457ccf035794644d08239f

Fix incorrect clicks in skill tests.
Fix a bug introduced by c7d911a49c2d that resulted in the
button being clicked incorrectly during the first challenge
in skill tests like
https://www.duolingo.com/skill/es/Shopping-3/test.

Fixes #14.

content.js
@@ -298,7 +298,13 @@ class ButtonClicker {
       isSkillTest &&
       this.numCorrectClicks == 0 &&
       buttonColor == greenButtonColor &&
-      findElements('div', e => e.getAttribute('data-test') == 'skill-icon')
+      // There are divs on the start screen with their data-test attributes set
+      // to 'skill-icon' and 'level-crown', but they unfortunately appear to
+      // remain in the DOM after the lesson is started.
+      findElements('div', e => {
+        const attr = e.getAttribute('data-test');
+        return attr && attr.split(' ').indexOf('challenge') != -1;
+      }).length == 0
     ) {
       console.log('Skipping skill test start screen');
       this.nextButton.click();